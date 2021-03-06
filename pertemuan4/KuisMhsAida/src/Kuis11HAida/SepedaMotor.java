a8edcd5ec8b18cad5096e94450081dff7e1988be
Aida Millati Mardiana <aidadiana2702@gmail.com>
2021-03-06 08:56:39 +0700

    update

---
+++ pertemuan4/KuisMhsAida/src/Kuis11HAida/SepedaMotor.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kuis11HAida;
+
+/**
+ *
+ * @author HP
+ */
+public class SepedaMotor {
+    String merk;
+    boolean kontakMotor = false;
+    int kecepatanMotor;
+    
+    SepedaMotor(){
+    }
+    
+    SepedaMotor(String merk){
+        this.merk = merk;
+    }
+    
+    void nyalakanMotor(boolean kontak){
+        
+        
+    }
+    
+    void matikanMotor(boolean kontak){
+       
+    }
+    
+    void tambahKecepatanMotor(int kec){
+        
+        
+    }
+    
+    void kurangiKecepatanMotor(int kec){
+        
+    }
+    
+    void info(){
+        
+    }
+}
---
+++ pertemuan4/KuisMhsAida/src/Kuis11HAida/SepedaMotorMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kuis11HAida;
+
+/**
+ *
+ * @author HP
+ */
+import java.util.Scanner;
+public class SepedaMotorMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SepedaMotor[] spd = new SepedaMotor[3];
+        spd[0] = new SepedaMotor("Honda");
+        spd[1] = new SepedaMotor("Yamaha");
+        spd[2] = new SepedaMotor("Ducati");
+        spd[3] = new SepedaMotor("Suzuki");
+        
+      
+    }
+    
+}
